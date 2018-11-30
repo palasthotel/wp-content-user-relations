513f0a99255930761e53dc03a3c08ab3b10a5181
EdwardBock <edward.bock@palasthotel.de>
2018-11-30 16:22:52 +0100

    format

--- inc/database/query.php
+++ inc/database/query.php
@@ -9,28 +9,32 @@
 namespace ContentUserRelations\Database;
 
 
+use ContentUserRelations\Plugin;
+
 class Query {
 
 	private $args = array();
-	private $relations = null;
+	private $relations = NULL;
 
 	/**
 	 * Query constructor.
 	 *
 	 * @param [user_id, WP_User] $args
 	 */
-	public function __construct($args) {
+	public function __construct( $args ) {
 		$this->args = $args;
 	}
 
 	/**
 	 * get user data
+	 *
 	 * @return array [relation1, relation2, ...]
 	 */
-	function get(){
-		if($this->relations == null){
+	function get() {
+		if ( $this->relations == NULL ) {
 			$this->relations = $this->execute();
 		}
+
 		return $this->relations;
 	}
 
@@ -39,60 +43,69 @@ class Query {
 	 *
 	 * @return array [propValue1=>[relation,...], ...]
 	 */
-	function getMapBy($prop){
+	function getMapBy( $prop ) {
 		$map = array();
-		foreach($this->get() as $relation){
-			if(!isset($map[$relation->{$prop}])) $map[$relation->{$prop}] = array();
-			$map[$relation->{$prop}][] = $relation;
+		foreach ( $this->get() as $relation ) {
+			if ( ! isset( $map[ $relation->{$prop} ] ) ) {
+				$map[ $relation->{$prop} ] = array();
+			}
+			$map[ $relation->{$prop} ][] = $relation;
 		}
-		return $map;
+
+		return apply_filters( Plugin::FILTER_QUERY_MAP_RESULT, $map, $prop);
 	}
 
 	/**
-	 * @return array ["type_slug" => [relation,...], "type_slug2" => [relation,...],...]
+	 * @return array ["type_slug" => [relation,...], "type_slug2" =>
+	 *     [relation,...],...]
 	 */
-	function getTypeMap(){
-		return $this->getMapBy("type_slug");
+	function getTypeMap() {
+		return $this->getMapBy( "type_slug" );
 	}
 
 	/**
 	 * @return array [userid => [relation,...],...]
 	 */
-	function getUserMap(){
-		return $this->getMapBy("user_id");
+	function getUserMap() {
+		return $this->getMapBy( "user_id" );
 	}
 
 	/**
 	 * get user data from db
+	 *
 	 * @return array
 	 */
-	private function execute(){
+	private function execute() {
 
 		// list of all available relations
-		if(isset($this->args["list"]) && true == $this->args["list"]){
+		if ( isset( $this->args["list"] ) && true == $this->args["list"] ) {
 			return getRelationTypeStatesList();
 		}
 
 		// get all states for type
-		if(isset($this->args["for_type"])){
-			return getRelationStates($this->args["for_type"]);
+		if ( isset( $this->args["for_type"] ) ) {
+			return getRelationStates( $this->args["for_type"] );
 		}
 
 		// get all types for state
-		if(isset($this->args["for_state"])){
-			return getRelationTypes($this->args["for_state"]);
+		if ( isset( $this->args["for_state"] ) ) {
+			return getRelationTypes( $this->args["for_state"] );
 		}
 
 		global $wpdb;
-		$queryConditions = new QueryConditions($this->args);
-		$conditions = $queryConditions->get_sql();
-		$where = "";
-		if("" != $conditions){
+		$queryConditions = new QueryConditions( $this->args );
+		$conditions      = $queryConditions->get_sql();
+		$where           = "";
+		if ( "" != $conditions ) {
 			$where = "WHERE $conditions";
 		}
 		$allRelationsSql = getAllRelationsSQL();
-		return $wpdb->get_results(
-			"SELECT * FROM ($allRelationsSql) as relations $where ORDER BY type_name, state_name"
+
+		return apply_filters(
+			\ContentUserRelations\Plugin::FILTER_QUERY_RESULT,
+			$wpdb->get_results(
+				"SELECT * FROM ($allRelationsSql) as relations $where ORDER BY type_name, state_name"
+			)
 		);
 	}
 }
